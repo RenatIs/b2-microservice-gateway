4a1ce8bbaa400eebeafbdefc00436968f563589c
Renatis <ismagilov89@gmail.com>
2020-12-06 10:19:14 +0200

    project clear

--- src/main/java/com/home/b2/microservice/gateway/b2microservicegateway/MainController.java
+++ src/main/java/com/home/b2/microservice/gateway/b2microservicegateway/MainController.java
@@ -1,6 +1,5 @@
 package com.home.b2.microservice.gateway.b2microservicegateway;
 
-import io.restassured.RestAssured;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,24 +8,8 @@ import java.time.Instant;
 @RestController
 public class MainController {
 
-    @GetMapping("/")
+    @GetMapping("/api")
     public String hello() {
         return "Hello from Gateway. Current time: " + Instant.now();
-    }
-
-    @GetMapping("/booking")
-    public String getBookingService() {
-        return RestAssured.given()
-                .baseUri("net-service-booking-service")
-                .log().all()
-                .get("/health").asString();
-    }
-
-    @GetMapping("/booking-bs")
-    public String getBookingServiceWithBS() {
-        return RestAssured.given()
-                .baseUri("net-service-booking-service/bs")
-                .log().all()
-                .get("/health").asString();
     }
 }
